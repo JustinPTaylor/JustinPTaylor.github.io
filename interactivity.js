5563b0cddf67a119d56eb614af2b0ade3ddc998a
Justin Taylor <taylor15@rose-hulman.edu>
2026-03-27 19:07:04 -0400

    Fixed another thing

--- interactivity.js
+++ interactivity.js
@@ -1,5 +1,4 @@
 function resizeIFrameToFitContent( iFrame ) {
-
     iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
     iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
 }
