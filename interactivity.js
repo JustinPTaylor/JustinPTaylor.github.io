128ccd81847022d2e7c4b3c7016106ccb5268ca5
Justin Taylor <taylor15@rose-hulman.edu>
2026-03-27 21:25:59 -0400

    simple buttons

---
+++ interactivity.js
@@ -0,0 +1,19 @@
+let nextButton = this.document.getElementById("next")
+let backButton = this.document.getElementById("back")
+
+nextButton.addEventListener("click", function() {
+    let first = document.getElementById("Project1");
+    let second = document.getElementById("Project2");
+    first.classList.add("hidden");
+    second.classList.remove("hidden");
+    nextButton.classList.add("hidden");
+    backButton.classList.remove("hidden");
+});
+backButton.addEventListener("click", function() {
+    let first = document.getElementById("Project1");
+    let second = document.getElementById("Project2");
+    second.classList.add("hidden");
+    first.classList.hidden("hidden");
+    backButton.classList.add("hidden");
+    nextButton.classList.remove("hidden");
+});
