61899ae368c4ae8630ae4d40ae9580e4e5637b3a
Justin Taylor <taylor15@rose-hulman.edu>
2026-03-27 22:43:08 -0400

    Image random generatoin test 1

--- indexButtons.js
+++ indexButtons.js
@@ -1,6 +1,51 @@
-let randomImage = this.document.getElementById("random-button");
-randomImage.addEventListener("click", function() {
+let randomImageBtn = this.document.getElementById("random-button");
+randomImageBtn.addEventListener("click", function() {
     let toRandomize = document.getElementById("main-image");
+    let oldRandomIndex = -1;
+    let waitTime = 1000;
+
     toRandomize.classList.remove("hidden");
-    toRandomize.src = "favorite_things_site.png";
+    
+    while (waitTime >= 0) {
+        oldRandomIndex = setTimeout(randomizeImage(toRandomize, oldRandomIndex), waitTime);
+        waitTime -= 100;
+    }
 });
+
+function randomizeImage(toRandomize, oldRandomIndex) {
+    let randomNum = Math.round(Math.random()*9);
+    while (randomNum == oldRandomIndex)
+        randomNum = Math.round(Math.random()*9);
+
+    if (randomNum == 0)
+        randomNum = 9;
+
+    if (randomNum = 1)
+        toRandomize.src = "images/self_photo.jpg";
+
+    if (randomNum = 2)
+        toRandomize.src = "images/self2.jpg";
+
+    if (randomNum = 3)
+        toRandomize.src = "images/self3.jpg";
+
+    if (randomNum = 4)
+        toRandomize.src = "images/self4.jpg";
+
+    if (randomNum = 5)
+        toRandomize.src = "images/self5.jpg";
+
+    if (randomNum = 6)
+        toRandomize.src = "images/self6.jpg";
+
+    if (randomNum = 7)
+        toRandomize.src = "images/self7.jpg";
+
+    if (randomNum = 8)
+        toRandomize.src = "images/self8.jpg";
+
+    if (randomNum = 9)
+        toRandomize.src = "images/self9.jpg";
+
+    return randomNum;
+}
